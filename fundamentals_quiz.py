14cf524edf8483cc0f253b79e19d085ffa9cf968
President-Asher <Asher.renzenbrink@gmail.com>
2023-10-05 09:30:11 -0400

    Ver 2.0

---
+++ fundamentals_quiz.py
@@ -0,0 +1,34 @@
+intro = "Hello World"
+print(intro)
+
+first_name = input("What is your first name?  ")
+print(first_name)
+
+age = 87
+temperature = 98.7
+too_hot = False
+
+print(type(age))
+print(type(temperature))
+print(type(too_hot))
+
+addition = age + temperature
+subraction = age - temperature
+multiplication = age * temperature
+division = age / temperature
+square = age ** temperature
+print(addition)
+print(subraction)
+print(multiplication)
+print(division)
+print(square)
+
+print(intro + first_name)
+print(f"{intro} {first_name}")
+
+def calculate_area(length, width):
+    print(length * width)
+    #return length * width
+
+calculate_area(10, 5)
+#print(calculate_area(10, 5))
